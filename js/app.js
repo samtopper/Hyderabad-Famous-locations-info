75b84f6e4c2a80d763a0f535b1e018ce6b6c5598
samtopper <sakhancse@gmail.com>
2016-12-13 18:43:15 +0530

    infowindow is displaying content correctly

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
-
 var map, marker;
 
 // executes when map fails to load.
 function loadingError() {
-    "use strict";
-    document.getElementById('error').innerHTML = "<h3>Google Maps is not loading. Please check your internet connection.</h3>";
+  "use strict";
+  $('#error').html(
+    "<h3>Google Maps is not loading. Please check your internet connection.</h3>"
+  );
 }
 
 //----ViewModel ---- //
 function initMap() {
   //API map constructor that creates new map.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 17.401481, lng: 78.450091},
+    center: {
+      lat: 17.401481,
+      lng: 78.450091
+    },
     zoom: 11
   });
 
@@ -21,8 +25,7 @@ function initMap() {
 
 }
 
-
-var ViewModel = function(){
+var ViewModel = function() {
   var self = this;
 
   //Make the model array into a knockout observable array.
@@ -35,58 +38,70 @@ var ViewModel = function(){
 
   // This function takes in a color, and then creates a new marker icon of that color.
   var markerImage;
+
   function makeMarkerIcon(markerColor) {
-   markerImage = new google.maps.MarkerImage(
-    'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-    '|40|_|%E2%80%A2',
-    new google.maps.Size(22, 35),  // 22 px wide by 35 px high.
-    new google.maps.Point(0, 0),   // origin to (0,0)
-    new google.maps.Point(10, 34),
-    new google.maps.Size(21,34));
+    markerImage = new google.maps.MarkerImage(
+      'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' +
+      markerColor +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(22, 35), // 22 px wide by 35 px high.
+      new google.maps.Point(0, 0), // origin to (0,0)
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21, 34));
     return markerImage;
   }
 
   self.observableLocations().forEach(function(item) {
     // Defining markers for each place.
     marker = new google.maps.Marker({
-        position: new google.maps.LatLng(item.position.lat, item.position.lng),
-        map: map,
-        animation: google.maps.Animation.DROP
+      position: new google.maps.LatLng(item.position.lat, item.position
+        .lng),
+      map: map,
+      animation: google.maps.Animation.DROP
     });
     item.marker = marker;
 
     // my Foursquare API keys. Create your own keys from https://developer.foursquare.com/
-    var fsquare_id = 'K3QM5R5HR0FLEUVDY2EU5PWVXL5TAGAC2EAKLVJ5UVZHSSDA';
-    var fsquare_secret = 'BU5ATIO30ETMIMAUGWVCXLBZGIMDQJAZ1ASLKT5NVURXS01W';
+    var fsquare_id =
+      'K3QM5R5HR0FLEUVDY2EU5PWVXL5TAGAC2EAKLVJ5UVZHSSDA';
+    var fsquare_secret =
+      'BU5ATIO30ETMIMAUGWVCXLBZGIMDQJAZ1ASLKT5NVURXS01W';
     // declaring the contentString as observable, so that every infowindow displays its content.
     var contentString = ko.observable();
     // AJAX request for foursquare data.
     $.ajax({
-        url: 'https://api.foursquare.com/v2/venues/explore',
-        dataType: 'jsonp',
-        type: "GET",
-        cache: 'false',
-        data: 'limit=1&ll=' + item.position.lat + ',' + item.position.lng + '&query=' + item.placeName + '&client_id=' + fsquare_id + '&client_secret=' + fsquare_secret + '&v=20140806&m=foursquare'
-    }).done(function(data){
-        !item.rating ? item.rating = data.response.groups[0].items[0].venue.rating : item.rating = "no rating available.";
-        console.log(item.rating);
-        !item.photos ? item.photos = data.response.groups[0].items[0].venue.photos : item.photos = "no photo to display.";
+      url: 'https://api.foursquare.com/v2/venues/explore',
+      dataType: 'jsonp',
+      type: "GET",
+      cache: 'false',
+      data: 'limit=1&ll=' + item.position.lat + ',' + item.position
+        .lng + '&query=' + item.placeName + '&client_id=' +
+        fsquare_id + '&client_secret=' + fsquare_secret +
+        '&v=20140806&m=foursquare'
+    }).done(function(data) {
+      !item.rating ? item.rating = data.response.groups[0].items[0]
+        .venue.rating : item.rating = "no rating available.";
+      console.log(item.rating);
+      // !item.photos ? item.photos = data.response.groups[0].items[0]
+      //   .venue.photos : item.photos = "no photo to display.";
 
-        // stores the content to be displayed on the infowindow.
-        contentString = '<br><div class="labels">' + '<div class="title">' + item.placeName + '</div><div class="photo">Photo: ' + item.photos + '</div><div class="rating">Foursquare rating: ' + item.rating + '</div><p>' + item.description + '</p>' + '</div>';
+      // stores the content to be displayed on the infowindow.
+      contentString = '<br><div class="labels">' +
+        '<div class="title">' + item.placeName +
+        '</div><div class="rating">Foursquare rating: ' + item.rating +
+        '</div><p>' + item.description + '</p>' + '</div>';
 
-
-        //self.populateInfoWindow(marker.content, infoWindow);
-    }).fail(function(jqXHR, textStatus){
+      //self.populateInfoWindow(marker.content, infoWindow);
+    }).fail(function(jqXHR, textStatus) {
       console.log("failed to get resources");
       //alert("failed to get resources from Foursquare.");
     });
 
-    google.maps.event.addListener(item.marker, 'click', function(){
+    google.maps.event.addListener(item.marker, 'click', function() {
       infoWindow.open(map, this);
 
       item.marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){
+      setTimeout(function() {
         item.marker.setAnimation(null);
       }, 1400);
 
@@ -108,12 +123,14 @@ var ViewModel = function(){
     // self.populateInfoWindow(clickedItem.marker, infoWindow);
   };
 
-//This stores the data in the input box into an observable string.
+  //This stores the data in the input box into an observable string.
   self.filter = ko.observable('');
-//function to filter the list!
+  //function to filter the list!
   self.filterList = ko.computed(function() {
-    return ko.utils.arrayFilter(self.observableLocations(), function(myPlace) {
-      var matched = myPlace.placeName.toLowerCase().indexOf(self.filter().toLowerCase()) >= 0;
+    return ko.utils.arrayFilter(self.observableLocations(), function(
+      myPlace) {
+      var matched = myPlace.placeName.toLowerCase().indexOf(self.filter()
+        .toLowerCase()) >= 0;
       myPlace.marker.setVisible(matched);
       return matched;
     });
@@ -122,10 +139,9 @@ var ViewModel = function(){
 };
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 }
-
